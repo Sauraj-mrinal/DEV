91518b71c4fbd0ba11f31172d3e5d30528e68e11
Sauraj-mrinal <msauraji@gmail.com>
2023-12-02 03:31:41 +0530

    Add

--- Day 9/Node_FileSystem_modul/index.js
+++ Day 9/Node_FileSystem_modul/index.js
@@ -41,10 +41,44 @@ console.log(content);
 //    this is my File Data ->hii my name is mrinal
 // and now try to leave the earth as soon as possible
 
+console.log('-------------------------------------------------------');
+// second way
+console.log(content.toString());
+// output-->
+// hii my name is mrinal
+// and now try to leave the earth as soon as possible
 
 
 
+// -----------------------------------------------------------
+// write FILE 
+//------------------------------------------------------------
+fileSystem.writeFileSync('write.txt' , 'this is file 2 data ')
+console.log('file Written');
 
+
+fileSystem.writeFileSync('here if we pass any new name which is not created , then it will auto be create' , 'this is file 2 data ')
+//if the file name pass does not exist then it will auto be create a new file with ur written name
+
+
+// ----------------------------------------------------------------------------------
+// update file contents -- or   Append to existing file 
+//------------------------------------------------------------
+
+fileSystem.appendFileSync('write.txt', 'this is file data updated');
+console.log('file Updated'); 
+
+
+
+//-------------delete file --------------------------------
+ // unlinkSync
+ //----------------------------------------------------------------
+
+ fileSystem.unlinkSync('filename.extension')
+
+ //-----------------------
+ // how we can delete the content of file --> we will see after some time 
+ 
 
 
 
