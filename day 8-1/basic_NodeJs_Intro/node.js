1cb74aa212da122d273f77fddcbc8c2d448f5a0b
Sauraj-mrinal <msauraji@gmail.com>
2023-12-01 20:20:53 +0530

    Add

---
+++ day 8-1/basic_NodeJs_Intro/node.js
@@ -0,0 +1,12 @@
+// if u want to use some module then u have to use require 
+
+const cp = require('child_process');
+// child_process module is node module while is use to create sub process inside a script
+
+console.log('Trying to open the calculator');
+
+cp.e
+
+
+
+
