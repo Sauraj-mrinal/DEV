80178e670581d987c5ad55a0848abe41402226f3
Sauraj-mrinal <msauraji@gmail.com>
2023-12-01 12:35:15 +0530

    Add

---
+++ day 8-1/index.js
@@ -0,0 +1,48 @@
+// java Script --Object 
+// object is in Key value pair 
+
+
+let obj = {} // how be delceler an object
+//-----------------------------
+// creation of an object 
+//--------------------------
+let persion = {
+    name: 'MRINAL',
+    gae: '25',
+    phone : '797976787',
+    gender: 'mail',
+    height: '5.7'
+
+}
+
+let car = {
+    name :'Ferrari',
+    Brand : 'Ferrari',
+    color:'blue',
+    price: '1000000000'
+}
+//-----------------------------
+// example 3 
+//-----------------------------
+
+// object for captain America 
+
+let cap = {
+    firstName: 'steve',
+    lastName: 'Rogers',
+
+    friends: ['Bucky' , 'John' , '  Doe' , 'Johndon'],
+    age:104,
+    isAverage: true,
+
+    // we can add adderss in side an object
+
+    Address :{
+        state: 'Manhattan',
+        city: 'San Francisco'
+    },
+    // we can write function in side the object 
+    sayHii: function(){
+        console.log("from function")
+    }
+}
