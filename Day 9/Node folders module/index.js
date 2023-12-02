b6302d5123de9ff204a67e49f0c6550788b569a5
Sauraj-mrinal <msauraji@gmail.com>
2023-12-03 00:27:04 +0530

    Add

--- Day 9/Node folders module/index.js
+++ Day 9/Node folders module/index.js
@@ -26,17 +26,17 @@ const fileSystem  = require('fs')
 // check if directory exists or not 
 
 // method --> existSync-->it will return true or false
-     let doesExist = fileSystem.existsSync('myDirectory')
+    // let doesExist = fileSystem.existsSync('myDirectory')
 
 
-console.log(doesExist);// false
+//console.log(doesExist);// false
 
         //------------status of directory
         //-------------------------------
         // method -->   lstatSync -->
 
-    let status = fileSystem.lstatSync('myDirectory')
-    console.log(status);
+    // let status = fileSystem.lstatSync('myDirectory')
+    // console.log(status);
 
 // output ---------->
 // Stats {
@@ -60,37 +60,53 @@ console.log(doesExist);// false
 //     birthtime: 2023-12-01T22:14:29.870Z
 //   }
 
-
+//----------------------------------------------------------------  --
 //---------we can use two method to fetch data from give upper output 
+//----------------------------------------------------------------------
 
-console.log('is File ?',status.isFile());
-console.log('is Directory',status.isDirectory());
+// console.log('is File ?',status.isFile());
+// console.log('is Directory',status.isDirectory());
 
 // output------>
 // is File ? false
 // is Directory true
 
-// readdirSync is A method which is use to see content inside a directory
+//---------------------------------------------------------------------------- 
+// readdirSync --> is A method which is use to see content inside a directory
+//----------------------------------------------------------------------------
 
-let folderPath = "file:///E:/notes_coding%20ninja/19.%20Notes%20-%20JQuery.pdf";
-
+// first we need folder path
+let folderPath = "F:\\pepcoding\\MERN";
+// we have to read the content of the folder 
 let folderContent = fileSystem.readdirSync(folderPath);
 
 console.log('Folder content: ' + folderContent);
-
-// Copying file From SRC to dest
+// it will show all the files in the folder.
+//-------------------------------------------------------------------------------------------------
+//                   Copying file From SRC to dest
+//-------------------------------------------------------------------------------------------------
+// source file address 
 
 let srcFilePath = "file:///E:/notes_coding%20ninja/19.%20Notes%20-%20JQuery.pdf";
 
+// destination file path 
+
 let destinationFilePath = "E:\pepDev\DEV\Day 9\Node folders module\myDirectory";
 
-let tobecopidedFilePath = path.basename(srcFilePath);
+// now first we find the name of the file 
 
+ let tobecopidedFilePath = path.basename(srcFilePath);
+// for example it will be some name 
+// now where we will past 
+
+// so first we will make destination path
+
+// we will forst make destination path 
 let destPath = path.join(destinationFolderPath , tobecopidedFileName);
+// so first here folder created and then put into copyFile 
+// fileSystem.copyFileSync(srcFilePath , desPath)
 
-fileSystem.copyFileSync(srcFilePath , desPath)
-
-console.log('File copied');
+// console.log('File copied');
 
 //  path: 'file:///E:/notes_coding%20ninja/19.%20Notes%20-%20JQuery.pdf'
 
