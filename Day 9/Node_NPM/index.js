8eaf11af1a9f949ac8d2220a30ecd9eedd9442d9
Sauraj-mrinal <msauraji@gmail.com>
2023-12-03 00:51:54 +0530

    ADD

--- Day 9/Node_NPM/index.js
+++ Day 9/Node_NPM/index.js
@@ -1,1 +1,13 @@
 // NPM - NODE PACKAGE MANAGER 
+
+const figlet = require('figlet');
+
+
+figlet("Mrinal", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  });
