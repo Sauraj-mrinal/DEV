d5bd7d9bea88ace62e3914f4a08d6e9f5016d0f9
Sauraj-mrinal <msauraji@gmail.com>
2023-12-04 03:08:39 +0530

    Add

--- day11/Small_project_Organizer/FO.js
+++ day11/Small_project_Organizer/FO.js
@@ -55,8 +55,33 @@ let input  =  process.argv.slice(2);
 let inputArray = input;
 console.log(inputArray);// ye zero index and 1 index ko chhor ke baki ko array me de dega as a input variable 
 
+let type ={
+    media: ["mp4", "mkv", "mp3"], 
+    archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
+    documents: [
+    "docx",
+    "doc",
+    "pdf",
+    "xlsx",
+    "xls",
+    "adt",
+    "ods",
+    "odp",
+    "odg",
+    "odf",
+    "txt",
+    "ps",
+    "tex"
+    ],
+    app: ["exe", "dmg" , "pkg", "deb"]
+}
+
+
+
+
 
 let command = inputArray[0];// to take command 
+
 // organize
 // [ 'organize' ]
 //Organize Implemented
@@ -89,9 +114,10 @@ function helpFun(){
 
 // Organize function will organize all your target folder's in a differnt  folder ACCORDING to thair extension
 
-function organizeFn(dirPath){
+function organizeFn(dirPath){// we need a directory path as a parameter
+
     //console.log(dirPath);
-     
+    let desPath;
 //
     if(dirPath==undefined){
         console.log('please enter a directory path');
@@ -102,7 +128,7 @@ function organizeFn(dirPath){
 
     // so if my directory esist => true then we have o make a 
     if(doesExist == true){
-       let  destPath = path.join(dirPath, 'Organized_Files')
+        destPath = path.join(dirPath, 'Organized_Files')
     //    path.join(dirPath,)
 // we created a path for orgnized Files Folder 
 
@@ -120,8 +146,61 @@ function organizeFn(dirPath){
     else{
         console.log('please enter a valid path');
     }
+    organizeHelper(dirPath , destPath);
 
 }
+
+// Day -11
+// when it call my all file will be organized. 
+function organizeHelper(src , dest){
+    // to get the content of the folder we need to write like .
+
+        let childName  = fs.readdirSync(src);
+
+
+        // we first identify file and folder separately 
+        console.log(childName); 
+
+        // output --> it is in the array form 
+        // [
+        //     'mca.pdf',
+        //     'newTxtFile.txt',
+        //     'Organized_Files',
+        //     'Pi7_Image_WhatsAppImage2023-11-14at11.24.08PM.jpeg',
+        //     'Postman-win64-Setup.exe',
+        //     'q24-delete-at-specified-index.zip'
+        //   ]
+
+        //------------------------------------------------------------------
+        //so we can use for loop 
+
+        for(let i=0; i<childName.length; i++) {
+           // so we find path first 
+           let childAddress = path.join(src, childName[i]);
+           console.log(childAddress);
+           console.log('--------------------------------');
+           // output
+            //E:\pepDev\DummyFolder\mca.pdf
+            // E:\pepDev\DummyFolder\newTxtFile.txt
+            // E:\pepDev\DummyFolder\Organized_Files
+            // E:\pepDev\DummyFolder\Pi7_Image_WhatsAppImage2023-11-14at11.24.08PM.jpeg
+            // E:\pepDev\DummyFolder\Postman-win64-Setup.exe
+            // E:\pepDev\DummyFolder\q24-delete-at-specified-index.zip
+
+            isFile = fs.lstatSync(childAddress).isFile();
+
+            if(isFile == true) {
+               let fileCategory = getCategory(childName[i]);
+            }
+        }
+}
+
+function getCategory(FileName){
+   let ext = path.extname(FileName).slice(1)
+   console.log(ext);
+
+}
+
 
 
 
